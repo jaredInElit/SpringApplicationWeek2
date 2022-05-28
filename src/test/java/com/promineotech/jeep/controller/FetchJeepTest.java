5ace36f59ec1d7366a5483ecc737332a4e7e5300
Jared <jaredboschmann@gmail.com>
2022-05-28 13:55:18 -0600

    first commit

---
+++ src/test/java/com/promineotech/jeep/controller/FetchJeepTest.java
@@ -0,0 +1,62 @@
+package com.promineotech.jeep.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import java.math.BigDecimal;
+import java.util.LinkedList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import com.promineotech.jeep.controller.support.FetchJeepTestSupport;
+import com.promineotech.jeep.entity.Jeep;
+import com.promineotech.jeep.entity.JeepModel;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+class FetchJeepTest extends FetchJeepTestSupport {
+
+  @Test
+  void testThatJeepsAreReturnedWhenAValidModelAndTrimAreSupplied() {
+
+    JeepModel model = JeepModel.WRANGLER;
+    String trim = "Sport";
+    String uri = String.format("%s?model=%s&trim=%s", getBaseUri(), model, trim);
+    
+    ResponseEntity<Jeep> response = getRestTemplate().getForEntity(uri, Jeep.class);
+    
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    
+    List<Jeep> expected = buildExpected();
+    assertThat(response.getBody()).isEqualTo(expected);
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+  
+  }
+  
+  
+ 
+  
+  protected List<Jeep> buildExpected(){
+    List<Jeep> list = new LinkedList<>();
+    
+    list.add(Jeep.builder()
+        .modelId(JeepModel.WRANGLER)
+        .trimLevel("Sport")
+        .numDoors(2)
+        .wheelSize(17)
+        .basePrice(new BigDecimal("28475.00"))
+        .build());
+    list.add(Jeep.builder()
+        .modelId(JeepModel.WRANGLER)
+        .trimLevel("Sport")
+        .numDoors(4)
+        .wheelSize(17)
+        .basePrice(new BigDecimal("31975.00"))
+        .build());
+    return list;
+  }
+
+}
